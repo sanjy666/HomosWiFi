2a02fbe3a4d6f097e9f770c415a15cf4a288332a
Sanjy <sanjy@feel-now.ru>
2019-11-19 05:47:43 +0500

    none

--- src/blynk_pin.h
+++ src/blynk_pin.h
@@ -1,199 +1,231 @@
 /* gpio
 
 */
-BLYNK_CONNECTED() {
-  rtc.begin();// Synchronize time on connection
-  Blynk.syncVirtual(V81,V82,V83,V84,V85,V86,V87,V88,V89,V90);
+BLYNK_CONNECTED()
+{
+  rtc.begin(); // Synchronize time on connection
+  Blynk.syncVirtual(V81, V82, V83, V84, V85, V86, V87, V88, V89, V90);
 }
 
-BLYNK_WRITE(V81) {
-worker[1].pin = param[0].asInt();
-worker[1].automatic = param[1].asInt();
-worker[1].temperature_sensor_index = param[2].asInt();
-worker[1].low_temp = param[3].asInt();
-worker[1].high_temp = param[4].asInt();
-worker[1].startTime = param[5].asInt();
-worker[1].stopTime = param[6].asInt();
-worker[1].run_time = param[7].asInt();
-worker[1].pause_time = param[8].asInt();
-worker[1].mode = param[9].asInt();
+/* BLYNK_WRITE(V80)
+{
+  pin_write(1, param.asInt());
+  terminal.println("wrire i2c 0 chanell");
+  terminal.print("read back = ");
+  terminal.println(pin_read(1));
+  terminal.print("last error = ");
+  terminal.println(gpio.lastError());
+  terminal.flush();
+} */
+
+BLYNK_WRITE(V81)
+{
+  worker[1].pin = param[0].asInt();
+  worker[1].automatic = param[1].asInt();
+  worker[1].temperature_sensor_index = param[2].asInt();
+  worker[1].low_temp = param[3].asInt();
+  worker[1].high_temp = param[4].asInt();
+  worker[1].startTime = param[5].asInt();
+  worker[1].stopTime = param[6].asInt();
+  worker[1].run_time = param[7].asInt();
+  worker[1].pause_time = param[8].asInt();
+  worker[1].mode = param[9].asInt();
 }
-BLYNK_WRITE(V82) {
-worker[2].pin = param[0].asInt();
-worker[2].automatic = param[1].asInt();
-worker[2].temperature_sensor_index = param[2].asInt();
-worker[2].low_temp = param[3].asInt();
-worker[2].high_temp = param[4].asInt();
-worker[2].startTime = param[5].asInt();
-worker[2].stopTime = param[6].asInt();
-worker[2].run_time = param[7].asInt();
-worker[2].pause_time = param[8].asInt();
-worker[2].mode = param[9].asInt();
+BLYNK_WRITE(V82)
+{
+  worker[2].pin = param[0].asInt();
+  worker[2].automatic = param[1].asInt();
+  worker[2].temperature_sensor_index = param[2].asInt();
+  worker[2].low_temp = param[3].asInt();
+  worker[2].high_temp = param[4].asInt();
+  worker[2].startTime = param[5].asInt();
+  worker[2].stopTime = param[6].asInt();
+  worker[2].run_time = param[7].asInt();
+  worker[2].pause_time = param[8].asInt();
+  worker[2].mode = param[9].asInt();
 }
-BLYNK_WRITE(V83) {
-worker[3].pin = param[0].asInt();
-worker[3].automatic = param[1].asInt();
-worker[3].temperature_sensor_index = param[2].asInt();
-worker[3].low_temp = param[3].asInt();
-worker[3].high_temp = param[4].asInt();
-worker[3].startTime = param[5].asInt();
-worker[3].stopTime = param[6].asInt();
-worker[3].run_time = param[7].asInt();
-worker[3].pause_time = param[8].asInt();
-worker[3].mode = param[9].asInt();
+BLYNK_WRITE(V83)
+{
+  worker[3].pin = param[0].asInt();
+  worker[3].automatic = param[1].asInt();
+  worker[3].temperature_sensor_index = param[2].asInt();
+  worker[3].low_temp = param[3].asInt();
+  worker[3].high_temp = param[4].asInt();
+  worker[3].startTime = param[5].asInt();
+  worker[3].stopTime = param[6].asInt();
+  worker[3].run_time = param[7].asInt();
+  worker[3].pause_time = param[8].asInt();
+  worker[3].mode = param[9].asInt();
 }
-BLYNK_WRITE(V84) {
-worker[4].pin = param[0].asInt();
-worker[4].automatic = param[1].asInt();
-worker[4].temperature_sensor_index = param[2].asInt();
-worker[4].low_temp = param[3].asInt();
-worker[4].high_temp = param[4].asInt();
-worker[4].startTime = param[5].asInt();
-worker[4].stopTime = param[6].asInt();
-worker[4].run_time = param[7].asInt();
-worker[4].pause_time = param[8].asInt();
-worker[4].mode = param[9].asInt();
+BLYNK_WRITE(V84)
+{
+  worker[4].pin = param[0].asInt();
+  worker[4].automatic = param[1].asInt();
+  worker[4].temperature_sensor_index = param[2].asInt();
+  worker[4].low_temp = param[3].asInt();
+  worker[4].high_temp = param[4].asInt();
+  worker[4].startTime = param[5].asInt();
+  worker[4].stopTime = param[6].asInt();
+  worker[4].run_time = param[7].asInt();
+  worker[4].pause_time = param[8].asInt();
+  worker[4].mode = param[9].asInt();
 }
-BLYNK_WRITE(V85) {
-worker[5].pin = param[0].asInt();
-worker[5].automatic = param[1].asInt();
-worker[5].temperature_sensor_index = param[2].asInt();
-worker[5].low_temp = param[3].asInt();
-worker[5].high_temp = param[4].asInt();
-worker[5].startTime = param[5].asInt();
-worker[5].stopTime = param[6].asInt();
-worker[5].run_time = param[7].asInt();
-worker[5].pause_time = param[8].asInt();
-worker[5].mode = param[9].asInt();
+BLYNK_WRITE(V85)
+{
+  worker[5].pin = param[0].asInt();
+  worker[5].automatic = param[1].asInt();
+  worker[5].temperature_sensor_index = param[2].asInt();
+  worker[5].low_temp = param[3].asInt();
+  worker[5].high_temp = param[4].asInt();
+  worker[5].startTime = param[5].asInt();
+  worker[5].stopTime = param[6].asInt();
+  worker[5].run_time = param[7].asInt();
+  worker[5].pause_time = param[8].asInt();
+  worker[5].mode = param[9].asInt();
 }
-BLYNK_WRITE(V86) {
-worker[6].pin = param[0].asInt();
-worker[6].automatic = param[1].asInt();
-worker[6].temperature_sensor_index = param[2].asInt();
-worker[6].low_temp = param[3].asInt();
-worker[6].high_temp = param[4].asInt();
-worker[6].startTime = param[5].asInt();
-worker[6].stopTime = param[6].asInt();
-worker[6].run_time = param[7].asInt();
-worker[6].pause_time = param[8].asInt();
-worker[6].mode = param[9].asInt();
+BLYNK_WRITE(V86)
+{
+  worker[6].pin = param[0].asInt();
+  worker[6].automatic = param[1].asInt();
+  worker[6].temperature_sensor_index = param[2].asInt();
+  worker[6].low_temp = param[3].asInt();
+  worker[6].high_temp = param[4].asInt();
+  worker[6].startTime = param[5].asInt();
+  worker[6].stopTime = param[6].asInt();
+  worker[6].run_time = param[7].asInt();
+  worker[6].pause_time = param[8].asInt();
+  worker[6].mode = param[9].asInt();
 }
-BLYNK_WRITE(V87) {
-worker[7].pin = param[0].asInt();
-worker[7].automatic = param[1].asInt();
-worker[7].temperature_sensor_index = param[2].asInt();
-worker[7].low_temp = param[3].asInt();
-worker[7].high_temp = param[4].asInt();
-worker[7].startTime = param[5].asInt();
-worker[7].stopTime = param[6].asInt();
-worker[7].run_time = param[7].asInt();
-worker[7].pause_time = param[8].asInt();
-worker[7].mode = param[9].asInt();
+BLYNK_WRITE(V87)
+{
+  worker[7].pin = param[0].asInt();
+  worker[7].automatic = param[1].asInt();
+  worker[7].temperature_sensor_index = param[2].asInt();
+  worker[7].low_temp = param[3].asInt();
+  worker[7].high_temp = param[4].asInt();
+  worker[7].startTime = param[5].asInt();
+  worker[7].stopTime = param[6].asInt();
+  worker[7].run_time = param[7].asInt();
+  worker[7].pause_time = param[8].asInt();
+  worker[7].mode = param[9].asInt();
 }
-BLYNK_WRITE(V88) {
-worker[8].pin = param[0].asInt();
-worker[8].automatic = param[1].asInt();
-worker[8].temperature_sensor_index = param[2].asInt();
-worker[8].low_temp = param[3].asInt();
-worker[8].high_temp = param[4].asInt();
-worker[8].startTime = param[5].asInt();
-worker[8].stopTime = param[6].asInt();
-worker[8].run_time = param[7].asInt();
-worker[8].pause_time = param[8].asInt();
-worker[8].mode = param[9].asInt();
+BLYNK_WRITE(V88)
+{
+  worker[8].pin = param[0].asInt();
+  worker[8].automatic = param[1].asInt();
+  worker[8].temperature_sensor_index = param[2].asInt();
+  worker[8].low_temp = param[3].asInt();
+  worker[8].high_temp = param[4].asInt();
+  worker[8].startTime = param[5].asInt();
+  worker[8].stopTime = param[6].asInt();
+  worker[8].run_time = param[7].asInt();
+  worker[8].pause_time = param[8].asInt();
+  worker[8].mode = param[9].asInt();
 }
-BLYNK_WRITE(V89) {
-worker[9].pin = param[0].asInt();
-worker[9].automatic = param[1].asInt();
-worker[9].temperature_sensor_index = param[2].asInt();
-worker[9].low_temp = param[3].asInt();
-worker[9].high_temp = param[4].asInt();
-worker[9].startTime = param[5].asInt();
-worker[9].stopTime = param[6].asInt();
-worker[9].run_time = param[7].asInt();
-worker[9].pause_time = param[8].asInt();
-worker[9].mode = param[9].asInt();
+BLYNK_WRITE(V89)
+{
+  worker[9].pin = param[0].asInt();
+  worker[9].automatic = param[1].asInt();
+  worker[9].temperature_sensor_index = param[2].asInt();
+  worker[9].low_temp = param[3].asInt();
+  worker[9].high_temp = param[4].asInt();
+  worker[9].startTime = param[5].asInt();
+  worker[9].stopTime = param[6].asInt();
+  worker[9].run_time = param[7].asInt();
+  worker[9].pause_time = param[8].asInt();
+  worker[9].mode = param[9].asInt();
 }
-BLYNK_WRITE(V90) {
-worker[10].pin = param[0].asInt();
-worker[10].automatic = param[1].asInt();
-worker[10].temperature_sensor_index = param[2].asInt();
-worker[10].low_temp = param[3].asInt();
-worker[10].high_temp = param[4].asInt();
-worker[10].startTime = param[5].asInt();
-worker[10].stopTime = param[6].asInt();
-worker[10].run_time = param[7].asInt();
-worker[10].pause_time = param[8].asInt();
-worker[10].mode = param[9].asInt();
+BLYNK_WRITE(V90)
+{
+  worker[10].pin = param[0].asInt();
+  worker[10].automatic = param[1].asInt();
+  worker[10].temperature_sensor_index = param[2].asInt();
+  worker[10].low_temp = param[3].asInt();
+  worker[10].high_temp = param[4].asInt();
+  worker[10].startTime = param[5].asInt();
+  worker[10].stopTime = param[6].asInt();
+  worker[10].run_time = param[7].asInt();
+  worker[10].pause_time = param[8].asInt();
+  worker[10].mode = param[9].asInt();
 }
-BLYNK_WRITE(V127) {
-  if (param.asInt() == 0) return;
-  Blynk.virtualWrite(V127,0);
+BLYNK_WRITE(V127)
+{
+  if (param.asInt() == 0)
+    return;
+  Blynk.virtualWrite(V127, 0);
   WiFiClient client;
   ESPhttpUpdate.setLedPin(2, LOW);
-	Blynk.disconnect();
-	delay(500);
+  Blynk.disconnect();
+  delay(500);
   Serial.println("Update started");
-	ESPhttpUpdate.update(client,String("http://192.168.1.200:8888/firmware.bin"));
+  ESPhttpUpdate.update(client, String("http://192.168.1.200:8888/firmware.bin"));
 }
 int select = 0;
 
-BLYNK_WRITE(V110){
-    select = param.asInt();
-    Blynk.virtualWrite(V111,worker[select].pin);
-    Blynk.virtualWrite(V112,pin_read(worker[select].pin));
-    Blynk.virtualWrite(V113,worker[select].automatic);
-    Blynk.virtualWrite(V114,worker[select].temperature_sensor_index);
-    //Blynk.virtualWrite(V115,temperatures[worker[select].temperature_sensor_index]);
-    Blynk.virtualWrite(V116,worker[select].low_temp);
-    Blynk.virtualWrite(V117,worker[select].high_temp);
-    Blynk.virtualWrite(V118,worker[select].startTime,worker[select].stopTime);
-    Blynk.virtualWrite(V119,worker[select].run_time,worker[select].pause_time);
-    Blynk.virtualWrite(V120,worker[select].mode);
-    Blynk.virtualWrite(
-        (V80+select),
-        worker[select].pin,
-        worker[select].automatic,
-        worker[select].temperature_sensor_index,
-        worker[select].low_temp,
-        worker[select].high_temp,
-        worker[select].startTime,
-        worker[select].stopTime,
-        worker[select].run_time,
-        worker[select].pause_time,
-        worker[select].mode
-    );
+BLYNK_WRITE(V110)
+{
+  select = param.asInt();
+  Blynk.virtualWrite(V111, worker[select].pin);
+  Blynk.virtualWrite(V112, pin_read(worker[select].pin));
+  Blynk.virtualWrite(V113, worker[select].automatic);
+  Blynk.virtualWrite(V114, worker[select].temperature_sensor_index);
+  //Blynk.virtualWrite(V115,temperatures[worker[select].temperature_sensor_index]);
+  Blynk.virtualWrite(V116, worker[select].low_temp);
+  Blynk.virtualWrite(V117, worker[select].high_temp);
+  Blynk.virtualWrite(V118, worker[select].startTime, worker[select].stopTime);
+  Blynk.virtualWrite(V119, worker[select].run_time, worker[select].pause_time);
+  Blynk.virtualWrite(V120, worker[select].mode);
+  Blynk.virtualWrite(
+      (V80 + select),
+      worker[select].pin,
+      worker[select].automatic,
+      worker[select].temperature_sensor_index,
+      worker[select].low_temp,
+      worker[select].high_temp,
+      worker[select].startTime,
+      worker[select].stopTime,
+      worker[select].run_time,
+      worker[select].pause_time,
+      worker[select].mode);
 }
-BLYNK_WRITE(V111){
-    worker[select].pin = param.asInt();
+BLYNK_WRITE(V111)
+{
+  worker[select].pin = param.asInt();
 }
-BLYNK_WRITE(V112){
-    pin_write(worker[select].pin,param.asInt());
+BLYNK_WRITE(V112)
+{
+  pin_write(worker[select].pin, param.asInt());
 }
-BLYNK_WRITE(V113){
-    worker[select].automatic = param.asInt();
+BLYNK_WRITE(V113)
+{
+  worker[select].automatic = param.asInt();
 }
-BLYNK_WRITE(V114){
-    worker[select].temperature_sensor_index = param.asInt();
+BLYNK_WRITE(V114)
+{
+  worker[select].temperature_sensor_index = param.asInt();
 }
-BLYNK_WRITE(V116){
-    worker[select].low_temp = param.asInt();
+BLYNK_WRITE(V116)
+{
+  worker[select].low_temp = param.asInt();
 }
-BLYNK_WRITE(V117){
-    worker[select].high_temp = param.asInt();
+BLYNK_WRITE(V117)
+{
+  worker[select].high_temp = param.asInt();
 }
-BLYNK_WRITE(V118){
-    TimeInputParam t(param);
-    if (t.hasStartTime())
-    {
-        worker[select].startTime = (t.getStartHour() * 60 * 60) + (t.getStartMinute() * 60) + t.getStartSecond();
-    }
-    if (t.hasStopTime())
-    {
-        worker[select].stopTime = (t.getStopHour() * 60 * 60) + (t.getStopMinute() * 60) + t.getStopSecond();
-    }
+BLYNK_WRITE(V118)
+{
+  TimeInputParam t(param);
+  if (t.hasStartTime())
+  {
+    worker[select].startTime = (t.getStartHour() * 60 * 60) + (t.getStartMinute() * 60) + t.getStartSecond();
+  }
+  if (t.hasStopTime())
+  {
+    worker[select].stopTime = (t.getStopHour() * 60 * 60) + (t.getStopMinute() * 60) + t.getStopSecond();
+  }
 }
-BLYNK_WRITE(V119){
+BLYNK_WRITE(V119)
+{
   TimeInputParam t(param);
   if (t.hasStartTime())
   {
@@ -204,7 +236,7 @@ BLYNK_WRITE(V119){
     worker[select].pause_time = (t.getStopHour() * 60 * 60) + (t.getStopMinute() * 60) + t.getStopSecond();
   }
 }
-BLYNK_WRITE(V120){
-    worker[select].mode = param.asInt();
+BLYNK_WRITE(V120)
+{
+  worker[select].mode = param.asInt();
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,9 @@
 #include <TimeLib.h>
 #include <WidgetRTC.h>
 
-
 //defines
 #define BLYNK_PRINT terminal
-//#define BLYNK_DEBUG
+//#define BLYNK_terminal.println
 #define ONEWIRE_BUS 12
 #define TEMPERATURE_PRECISION 10 // 12 9 or other ds18b20 resolution
 #define TEMPERATURE_START_PIN V100
@@ -20,10 +19,9 @@
 #define SCL_PIN 5
 #define EXPANDER_ADR 0x3c
 #define LAMP_PIN 16
-#define LAMP_SAVER_TIMER 1000*60*5
+#define LAMP_SAVER_TIMER 1000 * 60 * 5
 #define CASE_FAN_PIN 13
 #define HEATER_FAN_PIN 14
-#define DEBUG terminal.println
 
 //var
 #include "setting.h"
@@ -36,15 +34,16 @@ unsigned long interval = 10;
 float temperatures[11];
 String otaURL;
 
-struct{
+struct
+{
   int pin = 0;
   int mode = 0; // 1 time range; 2 heater; 3 cooler; 4 periodic;
   int automatic = 0;
-  float low_temp = 0,high_temp = 0;
+  float low_temp = 0, high_temp = 0;
   int temperature_sensor_index = -1;
-  long int startTime = 0,stopTime = 0;
-  long int run_time = 0,pause_time = 0;
-}worker[11];
+  long int startTime = 0, stopTime = 0;
+  long int run_time = 0, pause_time = 0;
+} worker[11];
 
 //class
 OneWire oneWire(ONEWIRE_BUS);
@@ -61,23 +60,25 @@ int time_check(long int start, long int stop);
 void worker_run(void);
 void pin_write(int pin, int value);
 void led_run(void);
+void i2c_scaner(void);
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("start");
+  terminal.println("start");
   Blynk.begin(auth, ssid, pass, address, port);
   sensorsInit();
   Wire.begin(SDA_PIN, SCL_PIN);
   setSyncInterval(10 * 60); //10 minute period RTC sync
-  timer.setInterval(500,worker_run);
-  timer.setInterval(10,led_run);
-  Serial.println("started");
-  pinMode(LAMP_PIN,OUTPUT);
-  pinMode(CASE_FAN_PIN,OUTPUT);
-  pinMode(HEATER_FAN_PIN,OUTPUT);
-  digitalWrite(CASE_FAN_PIN,LOW);
-  digitalWrite(HEATER_FAN_PIN,LOW);
+  timer.setInterval(1000, worker_run);
+  timer.setInterval(20, led_run);
+  //  timer.setInterval(5000,i2c_scaner);
+  terminal.println("started");
+  pinMode(LAMP_PIN, OUTPUT);
+  pinMode(CASE_FAN_PIN, OUTPUT);
+  pinMode(HEATER_FAN_PIN, OUTPUT);
+  digitalWrite(CASE_FAN_PIN, LOW);
+  digitalWrite(HEATER_FAN_PIN, LOW);
 }
 void loop()
 {
@@ -85,19 +86,23 @@ void loop()
   timer.run();
 }
 
-void sensorsRun(void){
+void sensorsRun(void)
+{
   float temperature = 0.0;
   int sensorsNum = sensors.getDeviceCount();
-  for (int i = 0; i < sensorsNum; i++){
+  for (int i = 0; i < sensorsNum; i++)
+  {
     temperature = sensors.getTempCByIndex(i);
-    if (temperature != 0.0 && temperature != -127){
-      temperatures[i+1] = temperature;
-      Blynk.virtualWrite( (TEMPERATURE_START_PIN + i) ,temperature);
+    if (temperature != 0.0 && temperature != -127)
+    {
+      temperatures[i + 1] = temperature;
+      Blynk.virtualWrite((TEMPERATURE_START_PIN + i), temperature);
     }
   }
   sensors.requestTemperatures();
 }
-void sensorsSetResolution(void){
+void sensorsSetResolution(void)
+{
   DeviceAddress tempDeviceAddress;
   int sensorsNum = sensors.getDeviceCount();
   for (int i = 0; i < sensorsNum; i++)
@@ -106,88 +111,119 @@ void sensorsSetResolution(void){
     sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
   }
 }
-void sensorsInit(void){
+void sensorsInit(void)
+{
   sensors.begin();
   sensors.setWaitForConversion(false);
   sensorsSetResolution();
   sensors.requestTemperatures();
   timer.setInterval(750 / (1 << (12 - TEMPERATURE_PRECISION)), sensorsRun);
 }
-int time_check(long int start, long int stop){
-  int nowtime = hour()*60*60 + minute()*60 + second();
-  if (start < stop){
-    if (start < nowtime && nowtime < stop){
+int time_check(long int start, long int stop)
+{
+  int nowtime = hour() * 60 * 60 + minute() * 60 + second();
+  if (start < stop)
+  {
+    if (start < nowtime && nowtime < stop)
+    {
       return 1;
-    }else{
+    }
+    else
+    {
       return 0;
     }
   }
-  if (start > stop){
-    if (stop < nowtime && nowtime < start){
+  if (start > stop)
+  {
+    if (stop < nowtime && nowtime < start)
+    {
       return 0;
-    }else{
+    }
+    else
+    {
       return 1;
     }
   }
   return 0;
 }
-void worker_run(void){
-  for (int i = 0; i < 10; i++){
-    if (worker[i].automatic != 0 && worker[i].pin != 0){
+void worker_run(void)
+{
+  for (int i = 0; i < 10; i++)
+  {
+    if (worker[i].automatic != 0 && worker[i].pin != 0)
+    {
       switch (worker[i].mode) // 1 time range; 2 heater; 3 cooler; 4 periodic;
       {
-        case 1: //time range
-          if (worker[i].startTime > 0 && worker[i].stopTime > 0 ){
-            pin_write(worker[i].pin, time_check(worker[i].startTime, worker[i].stopTime));
-          }
+      case 1: //time range
+        if (worker[i].startTime > 0 && worker[i].stopTime > 0)
+        {
+          pin_write(worker[i].pin, time_check(worker[i].startTime, worker[i].stopTime));
+        }
+        break;
+      case 2: // heater
+        if (worker[i].temperature_sensor_index < 0)
           break;
-        case 2: // heater
-          if (worker[i].temperature_sensor_index < 0) break;
-          //if (worker[i].low_temp == 0) break;
-          //if (worker[i].high_temp == 0) break;
-          //if (temperatures[i] == 0.0) break;
-          if (temperatures[worker[i].temperature_sensor_index] < worker[i].low_temp ){
-            pin_write(worker[i].pin, 1);
-          }else if (temperatures[worker[i].temperature_sensor_index] > worker[i].high_temp){
-            pin_write(worker[i].pin, 0);
-          }else{
-            pin_write(worker[i].pin, 0);
-          }
+        //if (worker[i].low_temp == 0) break;
+        //if (worker[i].high_temp == 0) break;
+        //if (temperatures[i] == 0.0) break;
+        if (temperatures[worker[i].temperature_sensor_index] < worker[i].low_temp)
+        {
+          pin_write(worker[i].pin, 1);
+        }
+        else if (temperatures[worker[i].temperature_sensor_index] > worker[i].high_temp)
+        {
+          pin_write(worker[i].pin, 0);
+          //          }else{
+          //            pin_write(worker[i].pin, 0);
+        }
+        break;
+      case 3: //cooler
+        if (worker[i].temperature_sensor_index < 0)
           break;
-        case 3: //cooler
-          if (worker[i].temperature_sensor_index < 0) break;
-          //if (worker[i].low_temp != 0) break;
-          //if (worker[i].high_temp != 0 ) break;
-          if (temperatures[worker[i].temperature_sensor_index] < worker[i].low_temp ){
-            pin_write(worker[i].pin, 0);
-          }else if (temperatures[worker[i].temperature_sensor_index] > worker[i].high_temp){
-            pin_write(worker[i].pin, 1);
-          }else{
-            pin_write(worker[i].pin, 0);
-          }
+        //if (worker[i].low_temp != 0) break;
+        //if (worker[i].high_temp != 0 ) break;
+        if (temperatures[worker[i].temperature_sensor_index] < worker[i].low_temp)
+        {
+          pin_write(worker[i].pin, 0);
+        }
+        else if (temperatures[worker[i].temperature_sensor_index] > worker[i].high_temp)
+        {
+          pin_write(worker[i].pin, 1);
+          //          }else{
+          //            pin_write(worker[i].pin, 0);
+        }
+        break;
+      case 4:
+      { //periodic
+        if (worker[i].run_time == 0)
           break;
-        case 4:{ //periodic
-          if (worker[i].run_time == 0) break;
-          if (worker[i].pause_time == 0) break;
-          long int all_times = worker[i].run_time + worker[i].pause_time;
-          int nowtimes = hour()*60*60 + minute()*60 + second();
-          if ((nowtimes % all_times) > worker[i].run_time){
-            pin_write(worker[i].pin, 0);
-          }else{
-            pin_write(worker[i].pin, 1);
-          }
+        if (worker[i].pause_time == 0)
           break;
-          }
-        default :
-          break;
+        long int all_times = worker[i].run_time + worker[i].pause_time;
+        int nowtimes = hour() * 60 * 60 + minute() * 60 + second();
+        if ((nowtimes % all_times) > worker[i].run_time)
+        {
+          pin_write(worker[i].pin, 0);
+        }
+        else
+        {
+          pin_write(worker[i].pin, 1);
+        }
+        break;
+      }
+      default:
+        break;
       }
     }
   }
 }
-void pin_write(int pin, int value){
-  switch (pin){
+void pin_write(int pin, int value)
+{
+  switch (pin)
+  {
   case 9:
-    if (millis() < LAMP_SAVER_TIMER){
+    if (millis() < LAMP_SAVER_TIMER)
+    {
       break;
     }
     digitalWrite(LAMP_PIN, value);
@@ -199,35 +235,85 @@ void pin_write(int pin, int value){
     digitalWrite(CASE_FAN_PIN, value);
     break;
   case 1:
-    gpio.write(0, ~value);
-    break;
+    if (value)
+    {
+      gpio.write(pin - 1, 0);
+    }
+    else
+    {
+      gpio.write(pin - 1, 1);
+    }
   case 2:
-    gpio.write(1, ~value);
-    break;
+    if (value)
+    {
+      gpio.write(pin - 1, 0);
+    }
+    else
+    {
+      gpio.write(pin - 1, 1);
+    }
   case 3:
-    gpio.write(2, ~value);
-    break;
+    if (value)
+    {
+      gpio.write(pin - 1, 0);
+    }
+    else
+    {
+      gpio.write(pin - 1, 1);
+    }
   case 4:
-    gpio.write(3, ~value);
-    break;
+    if (value)
+    {
+      gpio.write(pin - 1, 0);
+    }
+    else
+    {
+      gpio.write(pin - 1, 1);
+    }
   case 5:
-    gpio.write(4, ~value);
-    break;
+    if (value)
+    {
+      gpio.write(pin - 1, 0);
+    }
+    else
+    {
+      gpio.write(pin - 1, 1);
+    }
   case 6:
-    gpio.write(5, ~value);
-    break;
+    if (value)
+    {
+      gpio.write(pin - 1, 0);
+    }
+    else
+    {
+      gpio.write(pin - 1, 1);
+    }
   case 7:
-    gpio.write(6, ~value);
-    break;
+    if (value)
+    {
+      gpio.write(pin - 1, 0);
+    }
+    else
+    {
+      gpio.write(pin - 1, 1);
+    }
   case 8:
-    gpio.write(7, ~value);
-    break;
+    if (value)
+    {
+      gpio.write(pin - 1, 0);
+    }
+    else
+    {
+      gpio.write(pin - 1, 1);
+    }
   default:
     break;
   }
 }
-int pin_read(int pin){
-  switch (pin){
+int pin_read(int pin)
+{
+  switch (pin)
+  {
   case 9:
     return digitalRead(LAMP_PIN);
   case 10:
@@ -235,32 +321,92 @@ int pin_read(int pin){
   case 11:
     return digitalRead(CASE_FAN_PIN);
   case 1:
-    return ~gpio.read(0);
+    if (gpio.read(pin - 1))
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   case 2:
-    return ~gpio.read(1);
+    if (gpio.read(pin - 1))
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   case 3:
-    return ~gpio.read(2);
+    if (gpio.read(pin - 1))
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   case 4:
-    return ~gpio.read(3);
+    if (gpio.read(pin - 1))
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   case 5:
-    return ~gpio.read(4);
+    if (gpio.read(pin - 1))
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   case 6:
-    return ~gpio.read(5);
+    if (gpio.read(pin - 1))
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   case 7:
-    return ~gpio.read(6);
+    if (gpio.read(pin - 1))
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   case 8:
-    return ~gpio.read(7);
+    if (gpio.read(pin - 1))
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   default:
-    break;
+    return 0;
   }
 }
-void led_run(void){
-  if (millis() - previousMillis >= interval){
-    if (brightness <= 0 ){ // reverse the direction of the fading at the ends of the fade:
+void led_run(void)
+{
+  if (millis() - previousMillis >= interval)
+  {
+    if (brightness <= 0)
+    { // reverse the direction of the fading at the ends of the fade:
       brightness = 0;
       fadeAmount = -fadeAmount;
     }
-    if (brightness >=1024 ){ // reverse the direction of the fading at the ends of the fade:
+    if (brightness >= 1024)
+    { // reverse the direction of the fading at the ends of the fade:
       brightness = 1024;
       fadeAmount = -fadeAmount;
     }
@@ -270,3 +416,43 @@ void led_run(void){
   }
 }
 #include "blynk_pin.h"
+
+void i2c_scaner(void)
+{
+  byte error, address;
+  int nDevices;
+
+  terminal.println("Scanning...");
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++)
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      terminal.print("I2C device found at address 0x");
+      if (address < 16)
+        terminal.print("0");
+      terminal.print(address, HEX);
+      terminal.println("  !");
+
+      nDevices++;
+    }
+    else if (error == 4)
+    {
+      terminal.print("Unknown error at address 0x");
+      if (address < 16)
+        terminal.print("0");
+      terminal.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    terminal.println("No I2C devices found\n");
+  else
+    terminal.println("done\n");
+}
